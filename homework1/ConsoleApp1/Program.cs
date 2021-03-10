5484c3043d0406d641b688fa4d7f75d04984b48b
Miyuki021212 <2256757572@qq.com>
2021-03-10 11:45:52 +0800

    提交了一个新文件

---
+++ homework1/ConsoleApp1/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = 1;
+            double b = 1;
+            Console.Write("请输入第一个数字：");
+            string s1 = Console.ReadLine();
+            Console.Write("请输入第二个数字：");
+            string s2 = Console.ReadLine();
+            if (s1 != null)
+            {
+                a = Convert.ToDouble(s1);
+            }
+            else
+            {
+                Console.Write("数据异常");
+            }
+            if (s1 != null)
+            {
+                b = Convert.ToDouble(s2);
+            }
+            else
+            {
+                Console.Write("数据异常");
+            }
+            Console.Write("请输入一个运算符：");
+            string symbol = Console.ReadLine();
+            Console.Write("结果为：");
+            if (symbol=="+")
+            {
+                Console.WriteLine(a + b);
+            }
+            else if (symbol == "-")
+            {
+                Console.WriteLine(a - b);
+            }
+            else if (symbol == "*")
+            {
+                Console.WriteLine(a * b);
+            }
+            else if (symbol == "/")
+            {
+                Console.WriteLine(a / b);
+            }
+            else
+            {
+                Console.Write("数据异常");
+            }
+        }
+    }
+}
---
+++ homework1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.comboBox1.Items.Add("+");
+            this.comboBox1.Items.Add("-");
+            this.comboBox1.Items.Add("*");
+            this.comboBox1.Items.Add("/");
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double a = 1;
+            double b = 1;
+            string s1 = textBox1.Text;
+            string s2 = textBox2.Text;
+            if (s1 != null)
+            {
+                a = Convert.ToDouble(s1);
+            }
+            if (s2 != null)
+            {
+                b = Convert.ToDouble(s2);
+            }
+            switch (comboBox1.SelectedItem.ToString()) //获取选择的内容
+            {
+                case "+": textBox3.Text=Convert.ToString(a+b); break;
+                case "-": textBox3.Text = Convert.ToString(a - b); break;
+                case "*": textBox3.Text = Convert.ToString(a * b); break;
+                case "/": textBox3.Text = Convert.ToString(a / b); break;
+            }
+        }
+    }
+}
